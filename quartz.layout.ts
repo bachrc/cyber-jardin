e9642f6f33e85b544e146e4fe7284d86a364bd8d
bachrc <yohann.bacha@bachrc.net>
2025-11-04 12:59:42 +0100

    fix: plus de lisibilité

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,7 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({title: "Pages"}),
   ],
   right: [
     //Component.Graph(),
